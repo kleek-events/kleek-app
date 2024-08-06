52ebf74e3cb3315741af7ea579632e961ae2a019
tomtomdu73 <thomas.cosialls@gmail.com>
2024-08-06 16:46:03 -0500

    perf: add prettier packages

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Options} */
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  tabWidth: 2,
+  printWidth: 100,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
