326996536836a90d14665fde98fcf0bb2fed119d
tomtomdu73 <thomas.cosialls@gmail.com>
2024-08-10 18:57:02 -0500

    feat(siwe): configure siwe client

--- src/context/Web3Provider.tsx
+++ src/context/Web3Provider.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { State, WagmiProvider } from 'wagmi'
 
 import { wagmiConfig, projectId } from '@/config/wagmi'
+import { siweConfig } from '@/config/siwe'
 
 // Setup queryClient
 const queryClient = new QueryClient()
@@ -16,6 +17,7 @@ if (!projectId) throw new Error('Project ID is not defined')
 createWeb3Modal({
   wagmiConfig,
   projectId,
+  siweConfig,
   enableAnalytics: true, // Optional - defaults to your Cloud configuration
   enableOnramp: true, // Optional - false as default
   themeMode: 'light',
