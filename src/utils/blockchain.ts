0460d8668d8eeeb4761150fb1b8d441599e5d85d
tomtomdu73 <thomas.cosialls@gmail.com>
2024-08-17 18:50:58 -0500

    refactor: add blockchain utils

---
+++ src/utils/blockchain.ts
@@ -0,0 +1,59 @@
+import { Address } from 'viem'
+
+import ethereumLogo from '@/assets/tokens/ethereum.svg'
+import usdcLogo from '@/assets/tokens/usdc.svg'
+import usdtLogo from '@/assets/tokens/usdt.svg'
+
+/*** TOKENS ***/
+interface Token {
+  readonly name: string
+  readonly symbol: string
+  readonly address: {
+    readonly mainnet: Address
+    readonly testnet: Address
+  }
+  readonly decimals: number
+  readonly logo: string
+}
+
+export const DEPOSIT_TOKEN_ALLOWED: Token[] = [
+  {
+    name: 'usdc',
+    symbol: 'USDC',
+    address: {
+      mainnet: '0x',
+      testnet: '0x036cbd53842c5426634e7929541ec2318f3dcf7e',
+    },
+    decimals: 6,
+    logo: usdcLogo,
+  },
+  {
+    name: 'usdt',
+    symbol: 'USDT',
+    address: {
+      mainnet: '0x',
+      testnet: '0x',
+    },
+    decimals: 6,
+    logo: usdtLogo,
+  },
+  {
+    name: 'ethereum',
+    symbol: 'ETH',
+    address: {
+      mainnet: '0x',
+      testnet: '0x',
+    },
+    decimals: 18,
+    logo: ethereumLogo,
+  },
+]
+
+/*** CONTRACTS ***/
+export const KLEEK_PROXY_ADDRESS = process.env.NEXT_PUBLIC_KLEEK_PROXY_ADDRESS as Address
+
+export const KLEEK_SHARE_DEPOSIT_ADDRESS = process.env
+  .NEXT_PUBLIC_KLEEK_SHARE_DEPOSIT_ADDRESS as Address
+
+export const KLEEK_TRANSFER_DEPOSIT_ADDRESS = process.env
+  .NEXT_PUBLIC_KLEEK_TRANSFER_DEPOSIT_ADDRESS as Address
