a048aed1041809d6d263ca296ccc29baea0466e1
tomtomdu73 <thomas.cosialls@gmail.com>
2024-08-18 16:41:20 -0500

    fix(pinata): add group in json upload

--- src/app/api/pinata/json/route.ts
+++ src/app/api/pinata/json/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: NextRequest) {
   try {
     const data = await req.json()
 
-    const uploadData = await pinata.upload.json(data)
+    const uploadData = await pinata.upload.json(data).group(process.env.PINATA_GROUP_ID)
     return NextResponse.json(uploadData, { status: 200 })
   } catch (e) {
     console.log(e)
--- src/utils/pinata.ts
+++ src/utils/pinata.ts
@@ -4,5 +4,5 @@ export const PINATA_GATEWAY_URL = process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL
 
 export const pinata = new PinataSDK({
   pinataJwt: `${process.env.PINATA_JWT}`,
-  pinataGateway: `${process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL}`,
+  pinataGateway: `${PINATA_GATEWAY_URL}`,
 })
