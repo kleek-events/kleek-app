397496ecbb7e9683f3c93431052d1c26100882f1
tomtomdu73 <thomas.cosialls@gmail.com>
2024-08-18 16:36:44 -0500

    perf(pinata): use fetch to get ipfs metadata

--- src/utils/pinata.ts
+++ src/utils/pinata.ts
@@ -1,5 +1,7 @@
 import { PinataSDK } from 'pinata'
 
+export const PINATA_GATEWAY_URL = process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL
+
 export const pinata = new PinataSDK({
   pinataJwt: `${process.env.PINATA_JWT}`,
   pinataGateway: `${process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL}`,
