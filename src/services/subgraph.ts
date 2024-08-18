68e3c38bb72cf47384b69804deb7fee07c423597
tomtomdu73 <thomas.cosialls@gmail.com>
2024-08-18 00:31:23 -0500

    feat(subgraph): implement service to getEvent

--- src/services/subgraph.ts
+++ src/services/subgraph.ts
@@ -15,6 +15,7 @@ export async function getAllEvents() {
     body: JSON.stringify({
       query: `{
             eventCreateds(where: {endDate_gt: ${today}}, orderBy: timestamp, orderDirection: desc) {
+                id  
                 eventId
                 owner
                 contentUri
@@ -35,6 +36,33 @@ export async function getAllEvents() {
   return data.eventCreateds
 }
 
-function getEvent(id: string) {}
+export async function getEvent(id: number) {
+  const response = await fetch(subgraphUri, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `{
+              eventCreateds(where: {eventId: ${id}}) {
+                eventId
+                owner
+                contentUri
+                endDate
+                timestamp
+                transactionHash
+            }
+      }`,
+    }),
+  })
+
+  if (!response.ok) {
+    console.error('Failed to fetch record', response)
+    throw new Error('Failed to fetch records')
+  }
+
+  const { data } = await response.json()
+  return data.eventCreateds[0]
+}
 
 function getEventsByUser(user: string) {}
