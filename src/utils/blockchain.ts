b9cb9ca20b0c2f1e932f872eeb9ffe10d6c87fa3
tomtomdu73 <thomas.cosialls@gmail.com>
2024-08-20 02:13:05 -0500

    refactor: blockchain utils + add types

--- src/utils/blockchain.ts
+++ src/utils/blockchain.ts
@@ -3,59 +3,56 @@ import { Address } from 'viem'
 import ethereumLogo from '@/assets/tokens/ethereum.svg'
 import usdcLogo from '@/assets/tokens/usdc.svg'
 import usdtLogo from '@/assets/tokens/usdt.svg'
+import { Contracts, Token } from './types'
 
-export const USDC_ADDRESS_BASE_SEPOLIA = '0x036cbd53842c5426634e7929541ec2318f3dcf7e'
-
-/*** TOKENS ***/
-interface Token {
-  readonly name: string
-  readonly symbol: string
-  readonly address: {
-    readonly mainnet: Address
-    readonly testnet: Address
-  }
-  readonly decimals: number
-  readonly logo: string
+/*** CONTRACTS ***/
+export const contracts: Contracts = {
+  KLEEK_PROXY: {
+    address: {
+      base:
+        process.env.CHAIN_ENV === 'testnet' ? '0x' : '0x41D60e2D8AAFEeff97b0704Def4A35aba6582aF3',
+    },
+  },
+  KLEEK_SHARE_DEPOSIT: {
+    address: {
+      base:
+        process.env.CHAIN_ENV === 'testnet' ? '0x' : '0x8084071AE8A350cbecC1cdB29a45468E0e48B8dA',
+    },
+  },
+  KLEEK_TRANSFER_DEPOSIT: {
+    address: {
+      base: process.env.CHAIN_ENV === 'testnet' ? '0x' : '0x',
+    },
+  },
 }
-
+/*** TOKENS ***/
 export const DEPOSIT_TOKEN_ALLOWED: Token[] = [
   {
     name: 'usdc',
     symbol: 'USDC',
-    address: {
-      mainnet: '0x',
-      testnet: USDC_ADDRESS_BASE_SEPOLIA,
-    },
+    address:
+      process.env.CHAIN_ENV === 'testnet'
+        ? '0x036cbd53842c5426634e7929541ec2318f3dcf7e'
+        : '0x036cbd53842c5426634e7929541ec2318f3dcf7e',
     decimals: 6,
     logo: usdcLogo,
   },
   {
     name: 'usdt',
     symbol: 'USDT',
-    address: {
-      mainnet: '0x',
-      testnet: '0x',
-    },
+    address:
+      process.env.CHAIN_ENV === 'testnet'
+        ? '0x2340C88808dcE139B36864970074315BCb0c9Fe0'
+        : '0x2340C88808dcE139B36864970074315BCb0c9Fe0',
+
     decimals: 6,
     logo: usdtLogo,
   },
-  {
-    name: 'ethereum',
-    symbol: 'ETH',
-    address: {
-      mainnet: '0x',
-      testnet: '0x',
-    },
-    decimals: 18,
-    logo: ethereumLogo,
-  },
 ]
 
-/*** CONTRACTS ***/
-export const KLEEK_PROXY_ADDRESS = process.env.NEXT_PUBLIC_KLEEK_PROXY_ADDRESS as Address
+/*** HELPERS ***/
+export const getTokenByAddress = (address: Address) =>
+  DEPOSIT_TOKEN_ALLOWED.find((token) => token.address === address)
 
-export const KLEEK_SHARE_DEPOSIT_ADDRESS = process.env
-  .NEXT_PUBLIC_KLEEK_SHARE_DEPOSIT_ADDRESS as Address
-
-export const KLEEK_TRANSFER_DEPOSIT_ADDRESS = process.env
-  .NEXT_PUBLIC_KLEEK_TRANSFER_DEPOSIT_ADDRESS as Address
+export const getTokenByName = (name: string) =>
+  DEPOSIT_TOKEN_ALLOWED.find((token) => token.name === name)
---
+++ src/utils/types.ts
@@ -0,0 +1,22 @@
+import { Address } from 'viem'
+
+export interface Token {
+  readonly name: string
+  readonly symbol: string
+  readonly address: Address
+  readonly decimals: number
+  readonly logo: string
+}
+
+interface ContractDetails {
+  address: {
+    base: Address
+    }
+  }
+}
+
+export interface Contracts {
+  readonly KLEEK_PROXY: ContractDetails
+  readonly KLEEK_SHARE_DEPOSIT: ContractDetails
+  readonly KLEEK_TRANSFER_DEPOSIT: ContractDetails
+}
